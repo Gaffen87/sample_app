f90799f325982440c43c0537361869b4f231b0f8
Aske Lysgaard <askelysgaard@mac.lan>
2025-06-12 10:57:42 +0200

    user signup done

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "user not created on invalid input" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user:
+                                   { name: "test",
+                                     email: "invalid@address",
+                                     password: "short",
+                                     password_confirmation: "wrong"
+                                   } }
+    end
+    assert_response :unprocessable_entity
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors", 6
+  end
+
+  test "user created on valid input" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {
+        name: "Test",
+        email: "valid@address.com",
+        password: "validpass",
+        password_confirmation: "validpass"
+      } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+  end
+end
