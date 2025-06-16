ec1f19c4c535306c42d34e89476fbbbe2096b463
Aske Lysgaard <askelysgaard@mac.lan>
2025-06-16 20:40:20 +0200

    advanced login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  helper_method :current_user
-
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,43 @@
 module SessionsHelper
+  def current_user
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+  end
+
   def login(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
   def logout
+    forget(current_user)
     reset_session
     @current_user = nil
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
