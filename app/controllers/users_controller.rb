3d4d25b251f6d5cf9ebf1251c9f12520f4391687
Aske Lysgaard <askelysgaard@MacBook--Aske.local>
2025-06-18 14:54:06 +0200

    account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,20 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      login @user
-      flash[:success] = "Welcome #{@user.name}"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render "new", status: :unprocessable_entity
     end
@@ -67,6 +67,6 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-    current_user ? (redirect_to root_url, status: :see_other unless current_user.admin?) : (redirect_to login_url, status: :see_other)
+    current_user ? (redirect_to root_url, status: :see_other unless current_user&.admin?) : (redirect_to login_url, status: :see_other)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,35 +1,78 @@
 require "test_helper"
 
-class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "user not created on invalid input" do
-    get signup_path
+class UsersSignup < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+end
+
+class UsersSignupTest < UsersSignup
+  test "invalid signup information" do
     assert_no_difference "User.count" do
-      post users_path, params: { user:
-                                   { name: "test",
-                                     email: "invalid@address",
-                                     password: "short",
-                                     password_confirmation: "wrong"
-                                   } }
+      post users_path, params: { user: {
+        name: "",
+        email: "invalid@email",
+        password: "short",
+        password_confirmation: "wrong"
+      } }
     end
     assert_response :unprocessable_entity
     assert_template "users/new"
     assert_select "div#error_explanation"
-    assert_select "div.field_with_errors", 6
+    assert_select "div.field_with_errors"
   end
 
-  test "user created on valid input" do
-    get signup_path
+  test "valid signup information with account activation" do
     assert_difference "User.count", 1 do
       post users_path, params: { user: {
-        name: "Test",
-        email: "valid@address.com",
-        password: "validpass",
-        password_confirmation: "validpass"
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password"
       } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
+end
+
+class AccountActivationTest < UsersSignup
+  def setup
+    super
+    post users_path, params: { user: {
+      name: "Example User",
+      email: "user@example.com",
+      password: "password",
+      password_confirmation: "password"
+    } }
+    @user = assigns(:user)
+  end
+
+  test "should not be activated" do
+    assert_not @user.activated?
+  end
+
+  test "should be able to log in before account activation" do
+    log_in_as(@user)
+    assert_not logged_in?
+  end
+
+  test "should not be able to log in with invalid activation token" do
+    get edit_account_activation_path("invalid token", email: @user.email)
+    assert_not logged_in?
+  end
+
+  test "should not be able to log in with invalid email" do
+    get edit_account_activation_path(@user.activation_token, email: "wrong")
+    assert_not logged_in?
+  end
+
+  test "should log in succesfully with valid activation token and email" do
+    get edit_account_activation_path(@user.activation_token, email: @user.email)
+    assert @user.reload.activated?
     follow_redirect!
-    assert_template "users/show"
+    assert_template 'users/show'
     assert logged_in?
     assert_not flash.empty?
   end
+
 end
